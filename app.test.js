3cc38ccccf3724c244eaddac2c5d13986724aeef
Igor Carneiro <igor.carneiro.silva13@gmail.com>
2022-04-27 18:28:35 -0300

    Test coverae.

--- app.test.js
+++ app.test.js
@@ -2,8 +2,8 @@ const calc = require('./app')
 
 test('Add 1 + 2 results 3', sumTest)
 test('Add 1 - 2 results -1', subTest)
-test('Add 2 x 2 results 4', mulTest)
-test('Add 3 ^ 2 results 9', expTest)
+// test('Add 2 x 2 results 4', mulTest)
+// test('Add 3 ^ 2 results 9', expTest)
 
 function sumTest() {expect(calc.sum(1, 2)).toBe(3)}
 function subTest() {expect(calc.sub(1, 2)).toBe(-1)}
